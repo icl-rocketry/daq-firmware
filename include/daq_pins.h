f5cd75992e3970de57c8410dac8f4d6afddaa530
Luís Marques <lfsm01@hotmail.com>
2019-12-10 18:16:14 +0000

    added pin_definitions

---
+++ include/daq_pins.h
@@ -0,0 +1,36 @@
+#ifndef DAQ_PINS_H
+#define DAQ_PINS_H
+
+#include <Arduino.h>
+
+const uint8_t GND_PIN = 1;
+const uint8_t VDD = 2;
+const uint8_t RESET = 3;
+const uint8_t ANTENNA_POS = 4;
+const uint8_t ANTENNA_NTRL = 5;
+const uint8_t PTAP3 = 6;
+const uint8_t PTAP2 = 7;
+const uint8_t THRM_CPL4 = 8;
+const uint8_t PTAP1 = 9;
+const uint8_t THRM_CPL3 = 10;
+const uint8_t THRM_CPL2 = 11;
+const uint8_t THRM_CPL1 = 12;
+const uint8_t SD_CLK = 13;
+const uint8_t PYRO_CHANNEL_PIN = 14;
+const uint8_t SD_CS = 16;
+const uint8_t SD_MOSI = 23;
+const uint8_t SD_MISO = 24;
+const uint8_t BOOT = 25;
+const uint8_t PYRO_CHANNEL_CONT = 26;
+const uint8_t ADC_CS = 27;
+const uint8_t ADC_CLKOUT = 28;
+const uint8_t LED = 29;
+const uint8_t SCLK = 30;
+const uint8_t ADC_MISO = 31;
+const uint8_t SERV_SDA = 33;
+const uint8_t USB2UART_TX = 34;
+const uint8_t USB2UART_RX = 35;
+const uint8_t SERV_SCL = 36;
+const uint8_t ADC_MOSI = 37;
+
+#endif
