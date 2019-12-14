45c6c72a6338f4eef6bc1ae35ca8f8060c07ccf4
Luís Marques <lfsm01@hotmail.com>
2019-12-14 11:14:36 +0000

    note to self: Physical pin number = no bueno

--- include/daq_pins.h
+++ include/daq_pins.h
@@ -3,34 +3,37 @@
 
 #include <Arduino.h>
 
-const uint8_t GND_PIN = 1;
-const uint8_t VDD = 2;
-const uint8_t RESET = 3;
-const uint8_t ANTENNA_POS = 4;
-const uint8_t ANTENNA_NTRL = 5;
-const uint8_t PTAP3 = 6;
-const uint8_t PTAP2 = 7;
-const uint8_t THRM_CPL4 = 8;
-const uint8_t PTAP1 = 9;
-const uint8_t THRM_CPL3 = 10;
-const uint8_t THRM_CPL2 = 11;
-const uint8_t THRM_CPL1 = 12;
-const uint8_t SD_CLK = 13;
-const uint8_t PYRO_CHANNEL_PIN = 14;
-const uint8_t SD_CS = 16;
-const uint8_t SD_MOSI = 23;
-const uint8_t SD_MISO = 24;
-const uint8_t BOOT = 25;
-const uint8_t PYRO_CHANNEL_CONT = 26;
-const uint8_t ADC_CS = 27;
-const uint8_t ADC_CLKOUT = 28;
-const uint8_t LED = 29;
-const uint8_t SCLK = 30;
-const uint8_t ADC_MISO = 31;
-const uint8_t SERV_SDA = 33;
-const uint8_t USB2UART_TX = 34;
-const uint8_t USB2UART_RX = 35;
-const uint8_t SERV_SCL = 36;
-const uint8_t ADC_MOSI = 37;
+const uint8_t BOOT = 0;
+const uint8_t RX = 1;
+const uint8_t SD_MISO = 2;
+const uint8_t TX = 3;
+const uint8_t PYRO_CHANNEL_CONT = 4;
+const uint8_t LED = 5;
+const uint8_t PYRO_CHANNEL_PIN = 12;
+const uint8_t SD_CS = 13;
+const uint8_t SD_CLK = 14;
+const uint8_t SD_MOSI = 15;
+const uint8_t ADC_CS = 16;
+const uint8_t ADC_CLKOUT = 17;
+const uint8_t ADC_SCLK = 18;
+const uint8_t ADC_MISO = 19;
+const uint8_t SERVO_SDA = 21;
+const uint8_t SERVO_SCL = 22;
+const uint8_t ADC_MOSI = 23;
+const uint8_t TC_CS3 = 25;
+const uint8_t TC_CS2 = 26;
+const uint8_t TC_CS1 = 27;
+const uint8_t TC_CS4 = 32;
+const uint8_t PTAP1 = 33;
+const uint8_t PTAP3 = 34;
+const uint8_t PTAP2 = 35;
+
+
+
+
+
+
+
+
 
 #endif
