34ea50b74359808b33cb97854ae68e844f687aac
DanieleVBella <danielebella2000@gmail.com>
2020-02-01 02:08:58 +0000

    Removed superfluouous comments from main.cpp
    CoNciSenESs

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include "pyro.h"
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(5, OUTPUT);
   Serial.begin(115200);
   
@@ -13,14 +12,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  // Serial.print("Internal Temperature = ");
-  // Serial.println(readThermocoupleInternal());
-  // Serial.print("c = ");
-  // Serial.println(readThermocoupleCelsius(1));
-  // Serial.print("Pressure = ");
-  // Serial.println(readPtap(1));
-  delay(500);
   dataLoop(WIFIloop());
+  delay(500);
 }
